167c5128ea3ed57c80a4fc1f236d3b70d0ce8a44
Saul <saul.maos@gmail.com>
2021-03-11 20:51:08 -0600

    update dependencies

--- app/src/main/java/com/recodigo/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/recodigo/pokemonapp/MainActivity.kt
@@ -3,14 +3,9 @@ package com.recodigo.pokemonapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.recodigo.pokemonapp.data.Pokemon
 import com.recodigo.pokemonapp.ui.theme.PokemonAppTheme
 
