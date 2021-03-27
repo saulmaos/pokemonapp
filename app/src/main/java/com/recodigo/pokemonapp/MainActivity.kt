41fa158e0c9e958fa62e9665ce0ebf5ef654a5db
Saul <saul.maos@gmail.com>
2021-03-26 22:49:14 -0600

    add animation to images

--- app/src/main/java/com/recodigo/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/recodigo/pokemonapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.collection.LruCache
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -15,8 +17,12 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -123,11 +129,18 @@ fun PokemonItemCard(pokemon: Pokemon, cache: LruCache<String, DominantColors>) {
                     )
                 }
             }
+            var isVisible by remember { mutableStateOf(false) }
+            val alpha by animateFloatAsState(
+                targetValue = if (isVisible) 1f else 0f,
+                animationSpec = tween(durationMillis = 2000)
+            )
             CoilImage(
                 data = pokemon.imageUrl,
                 contentDescription = "pokemon",
+                onRequestCompleted = { isVisible = true },
                 modifier = Modifier
                     .size(70.dp)
+                    .alpha(alpha = alpha)
                     .constrainAs(image) {
                         end.linkTo(parent.end)
                     }
